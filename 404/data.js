1f7f9ebbb8e3958d0ec0c6cca0ad4b1a112c2c42
bincool <chengyiwang9303@foxmail.com>
2018-08-16 18:58:02 +0800

    Site updated: 2018-08-16 18:58:01

--- 404/data.js
+++
@@ -1,3 +0,0 @@
-var _base = 'https://qzone.qq.com/gy/404/';
-document.write('<script type="text/javascript" src="' + _base + 'data.js" charset="utf-8"></script>');
-document.write('<script type="text/javascript" src="'+ _base + 'page.js" charset="utf-8"></script>');
